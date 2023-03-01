aa4346f14a5ec4e4b9d7e3b506fb3e6c9bbfa2a0
Ahmadul Karim Chowdhury <ahmadulkc@gmail.com>
2023-03-01 22:10:27 +0600

    Add files via upload

---
+++ Semester 4-1/CSE 4130 Formal Languages & Compilers Lab/Session 01/practice.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+
+    return 0;
+}
+
+/* str.erase(std::remove(str.begin(), str.end(), '\t'), str.end());
+str.erase(std::remove(str.begin(), str.end(), '\n'), str.cend());
+remove(str.begin(), str.end(), ' ');
+remove(str.begin(), str.end(), '*');
+remove(str.begin(), str.end(), '/'); */
